c37a34990558c40daa28ca42d09620aba352df60
S Moosivand <saeed.m1981@gmail.com>
2026-01-22 14:50:57 +0000

    edit task3

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -1,15 +1,22 @@
-sgn x = if x < 0 
-          then -1 
-          else if x == 0 
-              then 0 
-              else 1
+grade score =
+ if score >= 90
+  then "A"
+  else if score >= 80
+   then "B"
+   else if score >= 70
+    then "C"
+    else "F"
 
-sgnGuards x | x < 0 = -1
-            | x == 0 = 0
-            | otherwise = 1
+
+grade score
+ | score >= 90 = "A"
+ | score >= 80 = "B"
+ | score >= 70 = "C"
+ | otherwise  = "F"
+
 
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (grade (x)) )
